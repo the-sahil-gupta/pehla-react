8c2cc7a05535b30a06dd4bd4d83b9da4c3bf1bb0
the-sahil-gupta <sahilgtech95@gmail.com>
2024-09-21 14:05:44 +0530

    data types

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,24 @@ import About from './components/About';
 import Home from './components/Home';
 import Nav from './components/Nav';
 
+let data = 'data from the server';
+let subjects = ['Math', 'English', 'Science'];
+let profile = {
+	name: 'John Doe',
+	age: 25,
+	email: 'johndoe99@gmail.com',
+};
+
 const App = () => {
 	return (
 		<div>
-			<Home />
-			<About />
-			<Nav />
+			<h1>{data}</h1>
+
+			<h1>{subjects}</h1>
+
+			<h1>{profile.name}</h1>
+			<h1>{profile.age}</h1>
+			<h1>{profile.email}</h1>
 		</div>
 	);
 };
