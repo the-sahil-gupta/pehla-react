feff17376a364b08827c3fdc62d89680ec10fcf4
the-sahil-gupta <sahilgtech95@gmail.com>
2024-09-24 17:35:10 +0530

    aadha aur krna he

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,81 @@
-import About from './components/About';
-import Home from './components/Home';
-import Nav from './components/Nav';
+// import About from './components/About';
+// import Home from './components/Home';
+// import Nav from './components/Nav';
 
-let data = 'data from the server';
-// String, Number, Boolean, NaN, null, undefined, Symbol
+// let data = 'data from the server';
+// // String, Number, Boolean, NaN, null, undefined, Symbol
 
-let subjects = ['Math', 'English', 'Science'];
-let profile = [
-	{
-		name: 'John Doe',
-		age: 25,
-		email: 'johndoe99@gmail.com',
-	},
-	{
-		name: 'John be',
-		age: 5,
-		email: 'johoe99@gmail.com',
-	},
-	{
-		name: 'JohDoe',
-		age: 2,
-		email: 'john9@gmail.com',
-	},
-];
+// let subjects = ['Math', 'English', 'Science'];
+// let profile = [
+// 	{
+// 		name: 'John Doe',
+// 		age: 25,
+// 		email: 'johndoe99@gmail.com',
+// 	},
+// 	{
+// 		name: 'John be',
+// 		age: 5,
+// 		email: 'johoe99@gmail.com',
+// 	},
+// 	{
+// 		name: 'JohDoe',
+// 		age: 2,
+// 		email: 'john9@gmail.com',
+// 	},
+// ];
 
-let x = profile.map((item, index) => {
-	return (
-		<div key={index}>
-			<h1>Name: {item.name}</h1>
-			<h2>Age: {item.age}</h2>
-			<p>Email: {item.email}</p>
-		</div>
-	);
-});
+// let x = profile.map((item, index) => {
+// 	return (
+// 		<div key={index}>
+// 			<h1>Name: {item.name}</h1>
+// 			<h2>Age: {item.age}</h2>
+// 			<p>Email: {item.email}</p>
+// 		</div>
+// 	);
+// });
 // console.log(x);
 
+// const App = () => {
+// 	return <div>{x}</div>;
+
+// return (
+// 	<div>
+// 		<Home></Home>
+// 		<Nav></Nav>
+// 		<About></About>
+// 	</div>
+// );
+
+// return (
+// 	<div>
+// 		<h1>{data}</h1>
+// 		<h1>{subjects}</h1>
+// 		<h1>{profile.name}</h1>
+// 		<h1>{profile.age}</h1>
+// 		<h1>{profile.email}</h1>
+// 	</div>
+// );
+// };
+
+const getInfo = () => {
+	console.log('Clicked');
+};
+const takeInfo = (info) => {
+	console.log(info);
+};
 const App = () => {
-	return <div>{x}</div>;
-
-	// return (
-	// 	<div>
-	// 		<Home></Home>
-	// 		<Nav></Nav>
-	// 		<About></About>
-	// 	</div>
-	// );
-
-	// return (
-	// 	<div>
-	// 		<h1>{data}</h1>
-	// 		<h1>{subjects}</h1>
-	// 		<h1>{profile.name}</h1>
-	// 		<h1>{profile.age}</h1>
-	// 		<h1>{profile.email}</h1>
-	// 	</div>
-	// );
+	return (
+		<div>
+			<button onClick={getInfo} className="bg-red-400 px-4 py-3 ">
+				Click me!
+			</button>
+			<button
+				onClick={() => takeInfo('take vala clicked')}
+				className="bg-red-400 px-4 py-3 mx-4 ">
+				Take vala clicke me
+			</button>
+		</div>
+	);
 };
 
 export default App;
